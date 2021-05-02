337b15034bcfa323619fdabee42991c9cdb437e4
Emmett Rushin <emmettrushin@gmail.com>
2021-05-02 15:26:23 -0400

    added functon to listen for click and listen for invalid input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,17 @@
 var requestURL = 'pro.openweathermap.org/data/2.5/forecast/hourly?q={city name}&appid={ab8da6ccc8f4bdca20182da7d0619b48}'
-var $search = $('#searchBtn')
-var $location = $('#location').value;
+var search = $('#searchBtn')
+var location = $('#location').value;
 
-$search.click(function() {
+$search.click('click', handleSubmission)
 
-})
+function handleSubmission (event) {
+    event.preventDefault();
+
+    if(!location) {
+        console.log('You need a search input value!');
+        return;
+    }
+
+    searchApi($location.value)
+
+}
