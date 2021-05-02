ef30ff9701bfff6cc4ce86a4adca7d5fb9704a3a
Emmett Rushin <emmettrushin@gmail.com>
2021-05-02 15:19:43 -0400

    changed input class name and added event listener on click

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+var requestURL = 'pro.openweathermap.org/data/2.5/forecast/hourly?q={city name}&appid={ab8da6ccc8f4bdca20182da7d0619b48}'
+var $search = $('#searchBtn')
+var $location = $('#location').value;
+
+$search.click(function() {
+
+})
