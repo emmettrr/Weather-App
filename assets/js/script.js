2aded135a9e0e319f9dec983d4bc3bd982602552
Emmett Rushin <emmettrushin@gmail.com>
2021-05-05 17:17:53 -0400

    added function to protect against false key

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,65 @@
-const apiKey = "2f4eb5b7c35ea26b6d2bd67436f4e25c"
+const apiKey = '2f4eb5b7c35ea26b6d2bd67436f4e25c'
 var search = $('#searchBtn')
 const inputVal = $('#locations').value;
-const url = `https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=${lat}&lon=${lon}&dt=${time}&appid=${apiKey}&units=metric`
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&units=imperial&appid=${apiKey}`
 
-search.click('click', handleSubmission)
+search.click(function weather(event){
+    $.ajax({
+        dataType: 'json',
+        url: url,
+        method: "GET",
+    }) .then(function (response){
+        console.log('AJAX response \n ------------')
+        console.log(response)
+        console.log("t", response.results.lengths)
+    })
+})
 
-function handleSubmission (event) {
-    event.preventDefault();
-
-    if(!inputVal) {
-        console.log(inputVal);
-        return;
+function isEmpty(obj) {
+    for(var key in obj) {
+        if(obj.hasOwnProperty(key)) {
+            return false; 
+        }
     }
-    weather();
+    return true; 
 }
 
+// function handleSubmission (event) {
+//     event.preventDefault();
+
+//     if(!inputVal) {
+//         console.log(inputVal);
+//         return;
+//     }
+//     weather();
+// }
 
 
-function weather (location) {
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
 
-        })
+
+// function weather (location) {
+//     fetch(url)
+//         .then(response => response.json())
+//         .then(data => {
+
+//         })
         
-        .then(function (localRes) {
-            var localRes = $('#searchHistory')
+//         .then(function (localRes) {
+//             var localRes = $('#searchHistory')
 
-            inputVal.textContent = localRes.search.query;
+//             inputVal.textContent = localRes.search.query;
 
-            if (!localRes.results.length) {
-                inputVal.innerHTML = '<h4>No city found, try again!</h4>';
-            } else {
-                inputVal.textContent = '';
-                for (let i = 0; i < localRes.results.length; i++) {
-                    printResults(localRes.results[i]);
+//             if (!localRes.results.length) {
+//                 inputVal.innerHTML = '<h4>No city found, try again!</h4>';
+//             } else {
+//                 inputVal.textContent = '';
+//                 for (let i = 0; i < localRes.results.length; i++) {
+//                     printResults(localRes.results[i]);
                     
-                }
-            }
-        })
-        .catch(() => {
-            msg.textContent = "Please search for a valid city!";
-          });
-}
+//                 }
+//             }
+//         })
+//         .catch(() => {
+//             msg.textContent = "Please search for a valid city!";
+//           });
+// }
