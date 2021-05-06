b7aee3ea7f922220338d35c1c2ba72a885210c30
Emmett Rushin <emmettrushin@gmail.com>
2021-05-06 12:57:29 -0400

    fixed search button function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,23 @@
 const apiKey = '2f4eb5b7c35ea26b6d2bd67436f4e25c'
-var search = $('#searchBtn')
 const inputVal = $('#locations')
 var units = '&units=imperial';
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&units=imperial&appid=${apiKey}`
+var url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&units=imperial&appid=${apiKey}`
 
-$('#searchBtn').click(function(){
-    fetch(url)
-    console.log(url,"hello")
-        .then(response => response.json())
-        .then(data => {
-            var temp = data['#currentTemp']
-            var wind = data['#currentWind']
-            var humid = data['#currentHumidity']
-            var uv = data['#currentUV']
 
-            inputVal.innerHTML = `City: ${'temp'}`
-        })
+$('#searchBtn').on('click',function(){
+    fetch(url+input.value+apiKey+units)
+    .then(function (response){
+        console.log(response.json());
+    })
 })
+        // .then (function(response) {
+        // (data => {
+        //     var temp = data['#currentTemp']
+        //     var wind = data['#currentWind']
+        //     var humid = data['#currentHumidity']
+        //     var uv = data['#currentUV']
+
+        //     inputVal.innerHTML = `City: ${'temp'}`
+        // })
+    
+// })
