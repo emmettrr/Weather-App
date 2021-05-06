4d85416f978ae0a0451e5c602e83059c73785c77
Emmett Rushin <emmettrushin@gmail.com>
2021-05-06 14:42:07 -0400

    finanlly got append to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,29 +2,25 @@ var apiKey = '&appid=2f4eb5b7c35ea26b6d2bd67436f4e25c'
 var input = document.getElementById('locations')
 var units = '&units=imperial';
 var url = `https://api.openweathermap.org/data/2.5/weather?q=`
+var name = document.getElementById('currentCity')
+var temp = document.getElementById('currentTemp')
+var wind = document.getElementById('currentWind')
+var humid = document.getElementById('currentHumidity')
 
 
 $('#searchBtn').on('click',function(){
     fetch(url+input.value+apiKey+units)
-    .then(function (response){
-        console.log(response.json());
-        console.log(response);
-    
-            (data => {
-            var name = data['#todayCurrentCity']
-            var temp = data['#currentTemp']
-            var wind = data['#currentWind']
-            var humid = data['#currentHumidity']
-            var uv = data['#currentUV']
+    .then(response => response.json())
+    .then(data => {
+        var currentName = data['name']
+        var currentTemp = data['main']['temp']
+        var currentWind = data['wind']['speed']
+        var currentHum = data['main']['humidity']
 
-            name.innerHTML = ` ${'name'}`.append(`${data.name}`)
-            temp.innerHTML = ` ${'temp'}`.append(`${data.main.temp}℉`)
-            wind.innerHTML = ` ${'wind'} mph`
-            humid.innerHTML = ` ${'humid'}`
-            uv.innerHTML = ` ${'#currentUV'}`
-        }
+        name.innerHTML = `${currentName}`
+        temp.innerHTML = `${currentTemp}`
+        wind.innerHTML = `${currentWind}`
+        humid.innerHTML = `${currentHum}`
+    })
         
-        )})
-
-    // .catch(err => alert("You need to enter a city name!"))
 })
