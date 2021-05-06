2f96b4edb779c81ae0fe91fbf27ad53bc41b8b47
Emmett Rushin <emmettrushin@gmail.com>
2021-05-06 17:08:37 -0400

    worked on pulling icon for weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 var apiKey = '&appid=2f4eb5b7c35ea26b6d2bd67436f4e25c'
 var input = document.getElementById('locations')
 var units = '&units=imperial';
-var icon = document.getElementById('icon')
+var icon = document.getElementById('weatherIcon')
 var fiveUrl = `https://api.openweathermap.org/data/2.5/forecast?q=`
 var city = document.getElementById('currentCity')
 var temp = document.getElementById('currentTemp')
@@ -20,9 +20,9 @@ $('#searchBtn').on('click',function(){
     .then(response => response.json())
     .then(data => {
         console.log(data)
-
+        
         var currentDate = moment().format('MMMM Do YYYY')
-        var currentIcon = data['list'][0]['weather'][0]['icon']
+        var currentIcon = "http://openweathermap.org/img/w/" + data['list'][0]['weather'][0]['icon'] + ".png"
         var currentCity = data['city']['name']
         var currentTemp = data['list'][0]['main']['temp']
         var currentWind = data['list'][0]['wind']['speed']
